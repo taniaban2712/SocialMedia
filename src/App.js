3a09d0fef9ebbe2df3ed4ba0b43fec5dbea5419b
Tania Banerjee <banerjeetania2019@gmail.com>
2023-07-22 02:40:59 +0530

    new commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/home/homepage";
+import React from "react";
+
+function App() {
+  return (
+     <Home/>
+  );
+}
+
+export default App;
---
+++ src/components/feed/feed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./feed.css"
+
+export default function Feed(){
+    return(
+        <div className='feed'>
+            <img src="/assets/others/ad.jpg" alt="" className="ad"/>
+            <div className="feedItem">
+            </div>
+        </div>
+    )
+}
